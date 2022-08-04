e8f1f7dd67b88f6ba8b729639819d08c95b29f99
yinye <yinye@021.com>
2022-08-04 14:39:12 +0800

    add bat file

---
+++ updateCharles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/CharlesUrlDecode.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/CharlesUrlDecodeText.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/TransactionViewerPopupMenu.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/CharlesUrlDecodeTextComponent.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/HttpUtils.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/FileUtils.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/JsonUtils.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/ResultDialog.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/BPDecode.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/BPDecodeText.class
+jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/BPDecodeTextComponent.class
