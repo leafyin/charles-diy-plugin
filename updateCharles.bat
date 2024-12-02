3be293eadccf531dec1b7fd70aa7215aa59fe013
leafyin <chinayinye@icloud.com>
2024-12-02 21:03:57 +0800

    charlse.jar包升级到4.6.5 更新时应该注意build后的.class文件路径

--- updateCharles.bat
+++ updateCharles.bat
@@ -1,6 +1,6 @@
-#!/bin/bash
-#将你需要的添加进jar包的文件写到下面
-#修改你需要更改的charles.jar包的地址，后面路径不需要改动
+@echo off
+::将你需要的添加进jar包的文件写到下面
+::修改你需要更改的charles.jar包的地址，后面路径不需要改动
 jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/CharlesUrlDecode.class
 jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/CharlesUrlDecodeText.class
 jar -uf E:/charles.jar com/xk72/charles/gui/transaction/popups/TransactionViewerPopupMenu.class
